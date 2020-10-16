de1fc8d5d5da7b71492784c114d14b34836bb5e0
kxwzhang <kxwzhang@gmail.com>
2020-10-16 16:57:34 -0700

    Remove BotKitConversation import

--- features/queries.js
+++ features/queries.js
@@ -1,9 +1,5 @@
-const { BotkitConversation } = require("botkit");
-
 module.exports = function(controller) {
 
-  let typing = new BotkitConversation('typing', controller);
-    
   controller.hears(async (message) => message.text && message.text.toLowerCase() === 'alex', ['message'], async (bot, message) => {
     await bot.reply(message, {type: 'typing'});
         setTimeout(async () => {
@@ -11,8 +7,9 @@ module.exports = function(controller) {
             await bot.changeContext(message.reference);
             await bot.reply(message, 'Hi! My name is Alex, what do you want to know about me?');
         }, 1000);
-    // await bot.reply(message, 'Hey! My name is Alex, what do you want to know about me?');
   });
+
+
 
 
 
