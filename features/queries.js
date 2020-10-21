9df7cdc04a006f6bdb84dcaca101de70ac1e2b68
kxwzhang <kxwzhang@gmail.com>
2020-10-20 17:08:39 -0700

    Generate sub keys for basics

--- features/queries.js
+++ features/queries.js
@@ -46,117 +46,143 @@ module.exports = function(controller) {
 
   }
 
+  function generateQuickReplies(resume, name) {
+    const quickReplies = Object.keys(resume).map((key) => {
+          let capKey = key.charAt(0).toUpperCase() + key.slice(1);
+          return { title: capKey, payload: `${name} ${capKey}` };
+    });
+    return quickReplies;
+  }
+
   // const names = ['kevin', 'winfred', 'alex'];
   // message.text.split(" ")[0].toLowerCase() => name from the message
   // names.includes(nameFromMessage)
-  controller.hears(async (message) => message.text && ['alex', 'winfred', 'kevin'].includes(message.text.split(" ")[0].toLowerCase()), ['message'], async (bot, message) => {
-    const [name, info] = message.text.split(' ');
-    !alexResume[info] ? alexResume.basics[info] : alexResume[info]; // refactor to run about/contact/location/profiles or bottom level
-    await bot.reply(message, {type: 'typing'});
-      setTimeout(async () => {
-        // will have to reset context because turn has now ended.
-        
-        await bot.changeContext(message.reference);
-        await bot.reply(message, JSON.stringify(alexResume.basics[info]));
-      }, 1000);
-  });
-
-  controller.hears(new RegExp('Alex Basics', 'i'), 'message', async (bot, message) => {
+  /**
+   * Receives an 'info' key. Check if it's a top-level key in the reusmes
+   * If it's not top-level, then key into resume[basics][info]
+   *['alex', 'winfred', 'kevin'].includes(message.text.split(" ")[0].toLowerCase())
+   */
+  controller.hears(
+    async (message) => message.text && new RegExp("winfred|kevin|alex", "i"),
+    ["message"],
+    async (bot, message) => {
+      let [name, info] = message.text.split(" ");
+      // console.log("message", message);
+      // console.log("info", info);
+      // console.log("alexResume[info]", alexResume[info]);
+      /*
     const person = JSON.stringify(message.text).replace(/['"]+/g, '');
-    const [personName, info] = person.split(' ');
-    // console.log(person.split(' '));
-
-
-
-    await bot.reply(message,{
-      text: `Here are some quick replies for Basics`,
-      quick_replies: [
-        {
-          title: 'About',
-          payload: `${personName} About`,
-        },
-        {
-          title: 'Contact',
-          payload: `${personName} Contact`,
-        },
-        {
-          title: 'Location',
-          payload: `${personName} Location`,
-        },
-        {
-          title: 'Profiles',
-          payload: `${personName} Profiles`,
+    const [personName, info] = person.split(" "); 
+    */
+      if (!info) { // just the name, no other info
+        // generate quick replies
+        // const quickReplies = Object.keys(alexResume).map((key) => {
+        //   let capKey = key.charAt(0).toUpperCase() + key.slice(1);
+        //   return { title: capKey, payload: `${name} ${capKey}` };
+        // });
+        // // bot response
+        // await bot.reply(message, {
+        //   text: `What would you like to learn about ${name}?`,
+        //   quick_replies: quickReplies,
+        // });
+        // bot response
+        await bot.reply(message, {
+          text: `What would you like to learn about ${name}?`,
+          quick_replies: generateQuickReplies(alexResume, name)
+        });
+      } else { // key into a resume, either top-level or inside basics
+          info = info.toLowerCase(); // somehow it's basics
+          console.log("info", info);
+          console.log("alexResume[info]", alexResume[info]);
+          console.log("alexResume.basics[info]", alexResume['basics'][info]);
+        // key into the 'basics' of resume
+        if (info === 'basics') {
+          // bot response ---> alex[about]
+          console.log('hitting info === basics');
+          await bot.reply(message, {
+            text: `What would you like to learn about ${name}?`,
+            quick_replies: generateQuickReplies(alexResume['basics'], name),
+          });
+        } else {
+          console.log('info !== basics I think');
+          await bot.reply(message, { type: "typing" });
+          setTimeout(async () => {
+            // will have to reset context because turn has now ended.
+            let response = alexResume[info]; // refactor to run about/contact/location/profiles or bottom level
+            await bot.changeContext(message.reference);
+            await bot.reply(message, Object.keys(JSON.parse(response)));
+          }, 1000);
         }
-      ]
-    });
-  });
+      }
+    }
+  );
+
+  // controller.hears(new RegExp('Alex Basics', 'i'), 'message', async (bot, message) => {
+  //   const person = JSON.stringify(message.text).replace(/['"]+/g, '');
+  //   const [personName, info] = person.split(' ');
+
+  //   await bot.reply(message,{
+  //     text: `Here are some quick replies for Basics`,
+  //     quick_replies: [
+  //       {
+  //         title: 'About',
+  //         payload: `${personName} About`,
+  //       },
+  //       {
+  //         title: 'Contact',
+  //         payload: `${personName} Contact`,
+  //       },
+  //       {
+  //         title: 'Location',
+  //         payload: `${personName} Location`,
+  //       },
+  //       {
+  //         title: 'Profiles',
+  //         payload: `${personName} Profiles`,
+  //       }
+  //     ]
+  //   });
+  // });
   
-  controller.hears(new RegExp('winfred|kevin|alex', 'i'), 'message', async (bot, message) => {
-    // Remove quotes
-    const person = JSON.stringify(message.text).replace(/['"]+/g, '');
-    const [personName] = person.split(" "); 
-    // check if the first word is their name
-    // if it's not, it won't show as "Person's Resume"
+  // controller.hears(new RegExp('winfred|kevin|alex', 'i'), 'message', async (bot, message) => {
+  //   // Remove quotes
+  //   const person = JSON.stringify(message.text).replace(/['"]+/g, '');
+  //   const [personName, info] = person.split(" "); 
+  //   // check if the first word is their name
+  //   // if it's not, it won't show as "Person's Resume"
 
-    // Text is "Alex"
-    // Text is "Alex Basics"
-    // Text is "Alex Basics About"
-    // { "about": "basics", "location": "basics" }
-    // alexResume[basics][about]
+  //   // Text is "Alex"
+  //   // Text is "Alex Basics"
+  //   // Text is "Alex Basics About"
+  //   // { "about": "basics", "location": "basics" }
+  //   // alexResume[basics][about]
 
-    // if personInfo length is 1, ask what info the user wants
-    // if personInfo length is 2, dive deeper into that info
+  //   // if personInfo length is 1, ask what info the user wants
+  //   // if personInfo length is 2, dive deeper into that info
 
-    // get keys of first layer to show as quick_replies
-    // key into each sub-layer and get all keys in that layer to display as quick replies
-    const quickReplies = Object.keys(alexResume)
-      .map(key => {
-        let capKey = key.charAt(0).toUpperCase() + key.slice(1);
-        return { title: capKey, payload: `${personName} ${capKey}`}
-      });
-    // map over the keys to make title and payload objects
-    // setting each element to an object with title/payload
-    // then set quick_replies to the array we made above
+  //   // get keys of first layer to show as quick_replies
+  //   // key into each sub-layer and get all keys in that layer to display as quick replies
+  //   const quickReplies = Object.keys(alexResume)
+  //     .map(key => {
+  //       let capKey = key.charAt(0).toUpperCase() + key.slice(1);
+  //       return { title: capKey, payload: `${personName} ${capKey}`}
+  //     });
+  //   // map over the keys to make title and payload objects
+  //   // setting each element to an object with title/payload
+  //   // then set quick_replies to the array we made above
     
 
-    //              Alex
-    //       Alex Basics
-    // Alex Basics About
+  //   //              Alex
+  //   //       Alex Basics
+  //   // Alex Basics About
 
-    await bot.reply(message, {
-      text: `What would you like to learn about ${personName}?`,
-      quick_replies: quickReplies
-      // quick_replies: [
-      //   {
-      //     title: 'Basics',
-      //     payload: `${personName} Basics`,
-      //   },
-      //   {
-      //     title: 'Work',
-      //     payload: `${personName} Work`,
-      //   },
-      //   {
-      //     title: 'Volunteer',
-      //     payload: `${personName} Volunteer`,
-      //   },
-      //   {
-      //     title: 'Education',
-      //     payload: `${personName} Education`,
-      //   },
-      //   {
-      //     title: 'Skills',
-      //     payload: `${personName} Skills`,
-      //   },
-      //   {
-      //     title: 'Languages',
-      //     payload: `${personName} Languages`,
-      //   },
-      //   {
-      //     title: 'Interests',
-      //     payload: `${personName} Interests`,
-      //   },
-      // ]
-    });
-  });
+  //   // if resume[info] is not null, then access resume[info] and do something with it
+  //   // else (so if null), then we know it's a key inside of basics. so we need to access resume[basics][info]
+
+  //   await bot.reply(message, {
+  //     text: `What would you like to learn about ${personName}?`,
+  //     quick_replies: quickReplies
+  //   });
+  // });
 
 }
